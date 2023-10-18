3c89e9725223bb6b4ce9dff205a991dff6215d29
Joris <jorisreijbroek@gmail.com>
2023-10-18 21:13:46 +0200

    tegel

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-export { default as Desnor } from "./src/de-snor.astro";
+export { default as Desnor } from "./src/desnor.astro";
 export { default as Knoepert } from "./src/knoepert.astro";
 export { default as Kop } from "./src/kop.astro";
 export { default as Lijstitem } from "./src/lijstitem.astro";
+export { default as Tegel } from "./src/tegel.astro";
