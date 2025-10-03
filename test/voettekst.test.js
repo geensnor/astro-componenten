b676c8373997d5141aeb657df772bc8ce9c38077
Joris <jorisreijbroek@gmail.com>
2025-10-03 12:56:52 +0200

    tests aangepast

--- test/voettekst.test.js
+++ test/voettekst.test.js
@@ -1,26 +1,19 @@
-import { expect } from "chai";
-import { getComponentOutput } from "astro-component-tester";
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { expect, test } from "vitest";
+import Kop from "../src/voettekst.astro";
+import Voettekst from "../src/voettekst.astro";
 
-describe("Voettekst test", () => {
-  // Simple test to get us started with the syntax
+const container = await AstroContainer.create();
+const result = await container.renderToString(Voettekst, {
+  props: {
+    githubURL: "https://www.geensnor.nl",
+  },
+});
 
-  // This show us how to write a test for our component's output using astro-component-tester
-  describe("Links", async () => {
-    let component;
+test("GitHub URL doet het", async () => {
+  expect(result).toContain("https://www.geensnor.nl");
+});
 
-    // First get the component's output, this returns an object containing the generated html (`.html`)
-    before(async () => {
-      component = await getComponentOutput("./src/voettekst.astro", {
-        githubURL: "https://www.github.com",
-      });
-    });
-
-    // Unless you modified /src/Component.astro, this should pass, as the component is empty apart from the frontmatter and new lines
-    it("Voettekst component bevat link naar geensnor.nl", () => {
-      expect(component.html).to.include("https://www.geensnor.nl");
-    });
-    it("Voettekst component bevat github link", () => {
-      expect(component.html).to.include("https://www.github.com");
-    });
-  });
+test("GitHub logo wordt getoond", async () => {
+  expect(result).toContain("GitHub-Mark-120px-plus.png");
 });
